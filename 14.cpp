0d588050be588e5371ba02a23583ef7fcac81c34
ishng <157577426+ishng@users.noreply.github.com>
2024-01-30 07:31:51 -0800

    Add files via upload

---
+++ 14.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int number;
+    cout <<"Enter the number of Month: ";
+    cin >>number;
+   if(number >=1 && number <=12) {
+   
+   if (number == 1) 
+    cout << "1 is January The Month of new Beginnings.\n\n";
+    else if (number == 2)
+    cout <<"2 is February The Month of Love.\n\n";
+     else if (number == 3)
+    cout <<"3 is March The Month of Graduation.\n\n"; 
+    else if (number == 4)
+    cout <<"4 is April The Month of summer.\n\n";
+    else if (number == 5)
+    cout <<"5 is May The Month of relaxation.\n\n";
+    else if (number == 6)
+    cout <<"6 is June The Month of classes.\n\n";
+    else if (number == 7)
+    cout <<"7 is July The Month of meeting.\n\n";
+    else if (number == 8)
+    cout <<"8 is August The Month of hardworking.\n\n";
+    else if (number == 9)
+    cout <<"9 is September The Month of exams.\n\n";
+    else if (number == 10)
+    cout <<"10 is October The Month of celebration.\n\n";
+    else if (number == 11)
+    cout <<"11 is November The Month of souls.\n\n";
+    else if (number == 12)
+    cout <<"12 is December The Month of giving.\n\n";
+    }
+    
+    else 
+    cout<<"Error! invalid Input.\n\n";
+    }
+
